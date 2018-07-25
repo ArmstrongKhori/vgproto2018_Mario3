ea6ff126d50a5744bc59b61de2763319f646a2cd
Mattwalsh155 <41699810+Mattwalsh155@users.noreply.github.com>
2018-07-25 14:06:14 -0700

    added items png

--- MarioBros3Project/example.js
+++ MarioBros3Project/example.js
@@ -9,9 +9,10 @@ il ~ "Image Loader" object. Use for loading pictures.
 
 // *** I am adding a new "Image" file that we can use later. I have id'd it "mario"... Remember that.
 il.AddTask("mario", "Mario.png");
+il.AddTask("Items", "Items.png");
 //
 // *** Now, I am creating "sprites" by "cutting out" parts of an image. Remember that image earlier? We're using that as reference!
-gm.AddSprite("smallMarioIdle", "mario", 64*0, 0, 64, 64, 1);
+gm.AddSprite("mushroom", "Items", 17, 209, 32, 32, 1);
 gm.AddSprite("smallMarioWalk", "mario", 64*1, 0, 64, 64, 2);
 gm.AddSprite("smallMarioRun", "mario", 64*3, 0, 64, 64, 2);
 // Parameters: "id for later use", "id of image we're using", source x, source y, source width, source height, number of frames
@@ -30,7 +31,7 @@ gm.AddSprite("level1background", "backdrop", 0, 432-256, 256, 256, 1);
 
 // *** We create an "actor"-- These are objects that can "interact" with the engine.
 var actor = gm.CreateActor(100, 100);
-actor.sprite = "smallMarioWalk"; // *** We're using Mario's "walking" sprite-- You know, the one we created earlier!
+actor.sprite = "mushroom"; // *** We're using Mario's "walking" sprite-- You know, the one we created earlier!
 actor.sprite_speed = 12/gm.frameRate; // *** Every "frame", this is how many frames we move forward in the sprite's animation. This code says "12 frames per second".
 
 
